585609ada84d78b8e94ce4bb50c6fef486142bd5
Sunidhi-21 <sunidhi.n.xworkz@gmail.com>
2020-12-05 14:45:02 +0530

    commit

---
+++ mathproject/MathOperationsImpl.java
@@ -0,0 +1,39 @@
+package com.xworkz.mathproject;
+
+public class MathOperationsImpl implements MathOperations {
+
+	@Override
+	public int addition(int a, int b) {
+
+		return a + b;
+	}
+
+	@Override
+	public int subtraction(int a, int b) {
+
+		return a - b;
+	}
+
+	@Override
+	public int multiplication(int a, int b) {
+
+		return a * b;
+	}
+
+	@Override
+	public int division(int a, int b) {
+
+		if (b != 0)
+			return a / b;
+		else
+			throw new RuntimeException("divisor should be a non-zero number");
+	}
+
+	@Override
+	public int modulus(int a, int b) {
+		if (b != 0)
+			return a % b;
+		else
+			throw new RuntimeException("divisor should be a non-zero number");
+	}
+}
