f2b9c7d8e16a48f0dc310d8fc5565f6e23c67242
N <Sunidhi.N@lumen.com>
2023-08-01 17:10:49 +0530

    changes made + new file added

--- mathproject/MathTester.java
+++ mathproject/MathTester.java
@@ -9,11 +9,11 @@ public class MathTester {
 		// TODO Auto-generated method stub
 
 		MathOperations mop = new MathOperationsImpl();
-		System.out.println("Addition of 2 nos: "+mop.addition(20, 10));
-		System.out.println("Subtraction of 2 nos: "+mop.subtraction(20, 10));
-		System.out.println("Multiplication of 2 nos: "+mop.multiplication(20, 10));
-		System.out.println("Division of 2 nos: "+mop.division(20, 0));
-		System.out.println("Modulus of 2 nos: "+mop.modulus(20, 0));
+		System.out.println("Addition of 2 no: "+mop.addition(20, 10));
+		System.out.println("Subtraction of 2 no: "+mop.subtraction(20, 10));
+		System.out.println("Multiplication of 2 no: "+mop.multiplication(20, 10));
+		System.out.println("Division of 2 no: "+mop.division(20, 0));
+		System.out.println("Modulus of 2 no: "+mop.modulus(20, 0));
 	}
 
 }
